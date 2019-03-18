2861a643d1c9e2e55d7b02e2d84b6cf76e86d734
arunkumardurgadevi <durgaarun.nm@gmail.com>
2019-03-18 15:30:20 +0530

    Create combined.c

---
+++ combined.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+char str1[1000],str2[1000],i,j;
+scanf("%s",str1);
+scanf("%s",str2);
+for(i=0;str1[i]!='\0';++i);
+for(j=0;str2[j]!='\0';++j,++i)
+{
+str1[i]=str2[j];
+}
+str1[i]='\0';
+printf("\n%s",str1);
+return 0;
+}
